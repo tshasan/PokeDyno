07f537615de33b9bd907829fb7cd19321f7706b3
Kazuma Saito <kazu15870@gmail.com>
2024-05-10 15:04:30 -0700

    feat: Retrived Pokemon detail through Poke API and displayed it on the html

--- src/main/java/com/pokedyno/controller/pokeController.java
+++ src/main/java/com/pokedyno/controller/pokeController.java
@@ -1,30 +1,47 @@
 package com.pokedyno.controller;
 
 import com.pokedyno.model.Pokemon;
+import com.pokedyno.model.PokemonDetail;
 import com.pokedyno.model.PokemonList;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class pokeController {
     private static final String POKEMON_API_URL = "https://pokeapi.co/api/v2";
     private final WebClient webClient;
 
     public pokeController(WebClient.Builder webClientBuilder) {
-        webClient = webClientBuilder.baseUrl(POKEMON_API_URL).build();
+        ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024)) // 10MB
+                .build();
+
+        webClient = webClientBuilder
+                .baseUrl(POKEMON_API_URL)
+                .exchangeStrategies(strategies)
+                .build();
     }
 
+    /**
+     * Get a list of Pokemon from the Pokemon API and fetch details for each Pokemon
+     *
+     * @param model Model to add the list of Pokemon to
+     * @return Thymeleaf template name
+     */
     @GetMapping("/")
-    public Mono<List<Pokemon>> getPokemonWebClient() {
-        int limit = 100;
+    public Mono<String> getPokemonWebClient(Model model) {
+        int limit = 50;
         int offset = 0;
 
-        return webClient.get()
+        Mono<List<Pokemon>> pokemonMono = webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/pokemon")
                         .queryParam("limit", limit)
@@ -34,5 +51,31 @@ public class pokeController {
                 .retrieve()
                 .bodyToMono(PokemonList.class)
                 .map(PokemonList::getPokemonList);
+
+        // Fetch details for each Pokemon
+        return pokemonMono.flatMapMany(Flux::fromIterable)
+                .concatMap(this::fetchPokemonDetails)
+                .collectList()
+                .doOnNext(pokemons -> model.addAttribute("pokemons", pokemons))
+                .thenReturn("pokemon");
+    }
+
+    /**
+     * Send a request to the Pokemon API to fetch details for a Pokemon
+     *
+     * @param pokemon Pokemon to fetch details for
+     * @return Pokemon with details
+     */
+    private Mono<Pokemon> fetchPokemonDetails(Pokemon pokemon) {
+        return webClient.get()
+                .uri(pokemon.getUrl())
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(PokemonDetail.class)
+                .map(pokemonDetail -> {
+                    pokemon.setId(pokemonDetail.getId());
+                    pokemon.setImage(pokemonDetail.getSprites().getOther().getOfficialArtwork().getFrontDefault());
+                    return pokemon;
+                });
     }
 }
--- src/main/java/com/pokedyno/model/Pokemon.java
+++ src/main/java/com/pokedyno/model/Pokemon.java
@@ -1,10 +1,20 @@
 package com.pokedyno.model;
 
 public class Pokemon {
+    private String id;
     private String name;
     private String url;
+    private String image;
 
     public Pokemon() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -21,5 +31,13 @@ public class Pokemon {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 }
